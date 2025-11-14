42b332773ad59470cae5f080b2e61bdb329e96c0
janindusanjana <janindusanjanagallage@gmail.com>
2025-11-14 13:32:50 +0530

    Gym management system java class and array list created

---
+++ GymManagementSystem.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class GymManagementSystem {
+    private ArrayList<Member> members = new ArrayList<>();
+    private ArrayList<Trainer> trainers = new ArrayList<>();
+    private ArrayList<Payment> payments = new ArrayList<>();
+}
